3ecb416f35d79e7a06880b997a84e37ef4cab851
DedMoroz38 <86836804+DedMoroz38@users.noreply.github.com>
2022-04-19 19:47:10 +0300

    Delete insertmap.js

--- insertmap.js
+++
@@ -1,8 +0,0 @@
-// let map = document.querySelector(".map");
-// let width = window.screen.width;
-// console.log(width);
-// if (width <= 420){
-//     map.style.cssText = `
-//         height: 311px !important;
-//     `;
-// } 
